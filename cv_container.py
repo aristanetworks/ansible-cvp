61b8d4f9fbdd4d8ec29c16b1f91a454ccab515e9
Hugh Adams <ha@arista.com>
2019-06-04 16:32:59 +0100

    Update Copyright message

--- cv_container.py
+++ cv_container.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2017, Arista Networks EOS+
+# Copyright (c) 2019, Arista Networks AS-EMEA
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
