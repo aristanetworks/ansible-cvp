0ec437103dc99653539a6e3dc6bb3eeed3a90aa5
Thomas Grimonet <thomas.grimonet@gmail.com>
2019-10-17 16:43:16 +0200

    Update cv-modules from Hugh's repo (#39)
    * Update cv-modules from Hugh's repo
    
    Backport changes made in https://github.com/Hugh-Adams/CVP_Ansible_Modules/commit/d7b23ef9edfd7d44e449609f18cfbc27b36f55c5
    Only add new changes.
    
    Add cvpinfo part of facts:
    
    "cvp_info": {
        "appVersion": "Foster_Build_03",
        "version": "2018.2.5"
    },
    
    	modified:   ../library/cv_device.py
    	modified:   ../library/cv_facts.py
    
    * Issue #17 - Remove cvprac reference
    
    Use code within module_utils instead of cvprac
    
    * Update facts structure
    
    Update structure to fit with structure expected by other modules
    It also register ansible_facts within ansible during the run.
    
    * Change target for lab
    
    * Issue #24 - Update cv_device
    
    Update content from Hugh's repository to make it idempotent
    
    Input example:
    ```
    - name: Test cv_device
      hosts: cvp
      connection: local
      gather_facts: no
      vars:
        configlet_list:
          cv_device_test01: "alias a{{ 999 | random }} show version"
          cv_device_test02: "alias a{{ 999 | random }} show version"
        # Device inventory for provision activity: bind configlet
        devices_inventory:
          veos01:
            name: veos01
            configlets:
              - cv_device_test01
              - SYS_TelemetryBuilderV2_172.23.0.2_1
              - veos01-basic-configuration
              - SYS_TelemetryBuilderV2
    ```
    
    Demo playbook part of the commit

--- library/cv_task.py
+++ library/cv_task.py
@@ -101,8 +101,8 @@ EXAMPLES="""
 
 import time
 from ansible.module_utils.basic import AnsibleModule
-from cvprac.cvp_client import CvpClient
-from cvprac.cvp_client_errors import CvpLoginError, CvpApiError
+from ansible.module_utils.cv_client import CvpClient
+from ansible.module_utils.cv_client_errors import CvpLoginError, CvpApiError
 
 
 def connect(module):
