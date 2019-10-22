289e0b2de60d63de43f8267124dacf94c35fbd30
Thomas Grimonet <tgrimonet@arista.com>
2019-10-22 19:30:03 +0200

    Resync branch

---
+++ arista/cvp/tests/collections/ansible_collections/arista/cvp/README.md
@@ -0,0 +1,160 @@
+![](https://img.shields.io/badge/Arista-CVP%20Automation-blue) ![GitHub](https://img.shields.io/github/license/aristanetworks/ansible-cvp)  ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/aristanetworks/ansible-cvp)  ![GitHub last commit](https://img.shields.io/github/last-commit/aristanetworks/ansible-cvp)
+
+<!-- @import "[TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+- [Ansible Modules for Arista CloudVision Platform](#ansible-modules-for-arista-cloudvision-platform)
+  - [About](#about)
+  - [Modules overview](#modules-overview)
+    - [Important notes.](#important-notes)
+  - [Installation](#installation)
+    - [Dependencies](#dependencies)
+    - [Git installation for testing](#git-installation-for-testing)
+    - [Git installation](#git-installation)
+  - [Example playbook](#example-playbook)
+  - [Resources](#resources)
+  - [License](#license)
+  - [Ask a question](#ask-a-question)
+  - [Contributing](#contributing)
+
+<!-- /code_chunk_output -->
+
+# Ansible Modules for Arista CloudVision Platform
+
+## About
+
+[Arista Networks](https://www.arista.com/) supports Ansible for managing devices running the EOS operating system through [CloudVision platform (CVP)](https://www.arista.com/en/products/eos/eos-cloudvision). This roles includes a set of ansible modules that perform specific configuration tasks on CVP server. These tasks include: collecting facts, managing configlets, containers, build provisionning topology and running tasks. For installation, you can refer to [specific section](#git-installation) of this readme.
+
+## Modules overview
+
+This repository provides content for Ansible's collection __arista.cvp__ with following content:
+
+- [__arista.cvp.cv_facts__](docs/cv_facts.md) - Collect CVP facts from server like list of containers, devices, configlet and tasks.
+- [__arista.cvp.cv_configlet__](docs/cv_configlet.md) -  Manage configlet configured on CVP.
+- [__arista.cvp.cv_container__](docs/cv_container.md) -  Manage container topology and attach configlet and devices to containers.
+- [__arista.cvp.cv_device__](docs/cv_device.md) - Manage devices configured on CVP
+- [__arista.cvp.cv_task__](docs/cv_tasks.md) - Run tasks created on CVP.
+
+This collection supports CVP version `2018.2.x` and `2019.1.x`
+
+### Important notes.
+
+This repository is built based on [new collections system](https://docs.ansible.com/ansible/devel/dev_guide/developing_collections.html#developing-collections) introduced by ansible starting version 2.9. 
+
+> It means that it is required to run at least ansible `2.9.0rc4` to be able to use this collection.
+
+## Installation
+
+### Dependencies
+
+This collection requires the following to be installed on the Ansible control machine:
+
+- ansible >= `2.9.0rc4`
+- requests >= `2.22.0`
+- fuzzywuzzy running `0.17.0` or later
+- treelib version `1.5.5` or later
+
+### Git installation for testing
+
+You can git clone this repository and use examples folder for testing. This folder contains a set of pre-configured playbook and ansible configuration:
+
+```shell
+$ git clone https://github.com/aristanetworks/ansible-cvp.git
+$ cd ansible-cvp/examples
+$ make build
+```
+
+> It is highly recommended to use a python virtual-environment to not alter your production environment.
+
+### Git installation
+
+You can git clone this repository and use examples folder for testing. This folder contains a set of pre-configured playbook and ansible configuration:
+
+__Clone repository__
+```shell
+$ git clone https://github.com/aristanetworks/ansible-cvp.git
+$ cd ansible-cvp
+```
+
+__Build and install collection__
+
+```shell
+$ ansible-galaxy collection build --force arista/cvp
+$ ansible-galaxy collection install arista.cvp.*.tar.gz
+```
+
+## Example playbook
+
+This example outlines how to use `arista.cvp` to create a containers topology on Arista CloudVision.
+
+Some playbook examples are provided in [__`examples`__](examples/) folder with information about how to built a test environment.
+
+Below is a very basic example to build a container tology on a CloudVision platform assuming you have 3 veos named `veos0{1,3}` and a configlet named `alias`
+
+```yaml
+---
+- name: Playbook to demonstrate cv_container module.
+  hosts: cvp
+  connection: local
+  gather_facts: no
+  collections:
+    - arista.cvp
+  vars:
+    containers_provision:
+        Fabric:
+          parent_container: Tenant
+        Spines:
+          parent_container: Fabric
+        Leaves:
+          parent_container: Fabric
+          configlets:
+              - alias
+          devices:
+            - veos03
+        MLAG01:
+          parent_container: Leaves
+          devices:
+            - veos01
+            - veos02
+  tasks:
+    - name: "Gather CVP facts from {{inventory_hostname}}"
+      cv_facts:
+        host: '{{ansible_host}}'
+        username: '{{cvp_username}}'
+        password: '{{cvp_password}}'
+        protocol: https
+        port: '{{cvp_port}}'
+      register: cvp_facts
+
+    - name: "Build Container topology on {{inventory_hostname}}"
+      cv_container:
+        host: '{{ansible_host}}'
+        username: '{{cvp_username}}'
+        password: '{{cvp_password}}'
+        port: '{{cvp_port}}'
+        protocol: https
+        topology: '{{containers_provision}}'
+        cvp_facts: '{{cvp_facts.ansible_facts}}'
+        save_topology: true
+```
+
+## Resources
+
+- Ansible for [Arista Validated Design](https://github.com/aristanetworks/ansible-avd)
+- Ansible [EOS modules](https://docs.ansible.com/ansible/latest/modules/list_of_network_modules.html#eos) on ansible documentation.
+- [CloudVision Platform](https://www.arista.com/en/products/eos/eos-cloudvision) overvierw
+
+## License
+
+Project is published under [Apache License](LICENSE).
+
+## Ask a question
+
+Support for this `arista.cvp` collection is provided by the community directly in this repository. Easiest way to get support is to open [an issue](https://github.com/aristanetworks/ansible-avd/issues).
+
+## Contributing
+
+Contributing pull requests are gladly welcomed for this repository. If you are planning a big change, please start a discussion first to make sure we’ll be able to merge it.
+
+You can also open an [issue](https://github.com/aristanetworks/ansible-avd/issues) to report any problem or to submit enhancement.
---
+++ arista/cvp/tests/collections/ansible_collections/arista/cvp/plugins/modules/cv_configlet.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, Arista Networks EOS+
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+#   Neither the name of Arista Networks nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARISTA NETWORKS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+ANSIBLE_METADATA = {'metadata_version': '0.5.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = """
+---
+module: cv_configlet
+version_added: "2.9"
+author: "EMEA AS (ansible-dev@arista.com)"
+short_description: Create, Delete, or Update CloudVision Portal Configlets.
+description:
+  - CloudVison Portal Configlet compares the list of configlets and config in
+  - configlets against cvp-facts then adds, deletes, or updates
+  - them as appropriate.
+  - If a configlet is in cvp_facts but not in configlets it will be deleted.
+  - If a configlet is in configlets but not in cvp_facts it will be created.
+  - If a configlet is in both configlets and cvp_facts it configuration will
+  - be compared and updated with the version in configlets
+  - if the two are different.
+options:
+  host:
+    description: IP Address or hostname of the CloudVisin Server
+    required: true
+    default: null
+  username:
+    description: The username to log into Cloudvision.
+    required: true
+    default: null
+  password:
+    description: The password to log into Cloudvision.
+    required: true
+    default: null
+  protocol:
+    description: The HTTP protocol to use. Choices http or https.
+    required: false
+    default: https
+  port:
+    description: The HTTP port to use. The cvprac defaults will be used
+                  if none is specified.
+    required: false
+    default: null
+  configlets:
+    description: List of configlets to managed on CVP server.
+    required: true
+    default: null
+  cvp_facts:
+    description: Facts extracted from CVP servers using cv_facts module
+    required: true
+    default: null
+  configlet_filter:
+    description: Filter to apply intended mode on a set of configlet.
+                 If not used, then module only uses ADD mode. configlet_filter
+                 list configlets that can be modified or deleted based
+                 on configlets entries.
+    required: false
+    default: null
+"""
+
+EXAMPLES = """
+---
+- name: Test cv_configlet_v2
+  hosts: cvp
+  connection: local
+  gather_facts: no
+  vars:
+    configlet_list:
+      Test_Configlet: "! This is a Very First Testing Configlet\n!"
+      Test_DYNAMIC_Configlet: "{{ lookup('file', 'templates/configlet_'+inventory_hostname+'.txt') }}"
+  tasks:
+    - name: 'Collecting facts from CVP {{inventory_hostname}}.'
+      tags:
+        - always
+      cv_facts:
+        host: '{{ansible_host}}'
+        username: '{{cvp_username}}'
+        password: '{{cvp_password}}'
+        protocol: https
+      register: cvp_facts
+
+    - name: 'Create configlets on CVP {{inventory_hostname}}.'
+      tags:
+        - provision
+      cv_configlet:
+        host: "{{ansible_host}}"
+        username: '{{cvp_username}}'
+        password: '{{cvp_password}}'
+        protocol: https
+        cvp_facts: "{{cvp_facts.ansible_facts}}"
+        configlets: "{{configlet_list}}"
+        configlet_filter: ["New", "Test","base-chk","base-firewall"]
+      register: cvp_configlet
+"""
+
+# Required by Ansible and CVP
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.arista.cvp.plugins.module_utils.cv_client import CvpClient
+from ansible_collections.arista.cvp.plugins.module_utils.cv_client_errors import CvpLoginError, CvpApiError
+import re
+from time import sleep
+# Required by compare function
+import difflib
+from fuzzywuzzy import fuzz # Library that uses Levenshtein Distance to calculate the differences between strings.
+
+def compare(fromText, toText, lines=10):
+    """ Compare text string in 'fromText' with 'toText' and produce
+        diffRatio - a score as a float in the range [0, 1] 2.0*M / T
+          T is the total number of elements in both sequences,
+          M is the number of matches.
+          Score - 1.0 if the sequences are identical, and 0.0 if they have nothing in common.
+        unified diff list
+          Code	Meaning
+          '- '	line unique to sequence 1
+          '+ '	line unique to sequence 2
+          '  '	line common to both sequences
+          '? '	line not present in either input sequence
+    """
+    fromlines = fromText.splitlines(1)
+    tolines = toText.splitlines(1)
+    diff = list(difflib.unified_diff(fromlines, tolines,n=lines))
+    textComp = difflib.SequenceMatcher(None, fromText, toText)
+    diffRatio = round( textComp.quick_ratio()*100, 2)
+    return [diffRatio,diff]
+
+def connect(module):
+    ''' Connects to CVP device using user provided credentials from playbook.
+    :param module: Ansible module with parameters and client connection.
+    :return: CvpClient object with connection instantiated.
+    '''
+    client = CvpClient()
+    try:
+        client.connect([module.params['host']],
+                       module.params['username'],
+                       module.params['password'],
+                       protocol=module.params['protocol'],
+                       port=module.params['port'],
+                       )
+    except CvpLoginError as e:
+        module.fail_json(msg=str(e))
+    return client
+
+def configlet_action(module):
+    ''' Compare configlets in "configlets" with configlets in "cvp_facts"
+    if configlet exists in "cvp_facts" check config, if changed update
+    if configlet does not exist in "cvp_facts" add to CVP
+    if configlet in "cvp_facts" but not in "configlets" remove from CVP if
+    not applied to a device or container.
+    :param module: Ansible module with parameters and client connection.
+    :return: data: dict of module actions and taskIDs
+    '''
+    # If any configlet changed updated 'changed' flag
+    changed = False
+    #Compare configlets against cvp_facts-configlets
+    keep_configlet = [] # configlets with no changes
+    delete_configlet = [] # configlets to delete from CVP
+    deleted = []
+    update_configlet = [] # configlets with config changes
+    updated = []
+    new_configlet = [] # configlets to add to CVP
+    new = []
+    taskList = [] # Tasks that have a pending status after function runs
+
+    for configlet in module.params['cvp_facts']['configlets']:
+        # Only deal with Static configlets not Configletbuilders or
+        # their derived configlets
+        # Include only configlets that match filter elements "all" will
+        # include all configlets.
+        if configlet['type'] == 'Static':
+            if re.search(r"\ball\b", str(module.params['configlet_filter'])) or (
+                any(element in configlet['name'] for element in module.params['configlet_filter'])):
+                if configlet['name'] in module.params['configlets']:
+                    ansible_configlet = module.params['configlets'][configlet['name']]
+                    configlet_compare = compare(configlet['config'],ansible_configlet)
+                    # compare function returns a floating point number
+                    if configlet_compare[0] == 100.0:
+                        keep_configlet.append(configlet)
+                    else:
+                        update_configlet.append({'data':configlet,'config':ansible_configlet})
+                else:
+                    delete_configlet.append(configlet)
+    # Look for new configlets, if a configlet is not CVP assume it is to be created
+    for ansible_configlet in module.params['configlets']:
+        found = False
+        for cvp_configlet in module.params['cvp_facts']['configlets']:
+            if str(ansible_configlet) == str(cvp_configlet['name']):
+                found = True
+        if not found:
+            new_configlet.append({'name':str(ansible_configlet),
+                                  'config':str(module.params['configlets'][ansible_configlet])})
+
+    # Only execute this section if ansible check_mode is false
+    if not module.check_mode:
+        # delete any configlets as required
+        if len(delete_configlet) > 0:
+            for configlet in delete_configlet:
+              try:
+                delete_resp =  module.client.api.delete_configlet(configlet['name'], configlet['key'])
+              except Exception as error:
+                errorMessage = re.split(':', str(error))[-1]
+                message = "Configlet %s cannot be deleted - %s"%(configlet['name'],errorMessage)
+                deleted.append({configlet['name']:message})
+              else:
+                if "error" in str(delete_resp).lower():
+                    message = "Configlet %s cannot be deleted - %s"%(configlet['name'],delete_resp['errorMessage'])
+                    deleted.append({configlet['name']:message})
+                else:
+                    changed = True
+                    deleted.append({configlet['name']:"success"})
+
+        # Update any configlets as required
+        if len(update_configlet) > 0:
+            for configlet in update_configlet:
+              try:
+                update_resp = module.client.api.update_configlet(configlet['config'],
+                                                                 configlet['data']['key'],
+                                                                 configlet['data']['name'])
+              except Exception as error:
+                errorMessage = re.split(':', str(error))[-1]
+                message = "Configlet %s cannot be updated - %s"%(configlet['name'],errorMessage)
+                updated.append({configlet['name']:message})
+              else:
+                if "errorMessage" in str(update_resp):
+                    message = "Configlet %s cannot be updated - %s"%(configlet['name'],update_resp['errorMessage'])
+                    updated.append({configlet['data']['name']:message})
+                else:
+                    module.client.api.add_note_to_configlet(configlet['data']['key'],"## Managed by Ansible ##")
+                    changed = True
+                    updated.append({configlet['data']['name']:"success"})
+
+        # Add any new configlets as required
+        if len(new_configlet) > 0:
+            for configlet in new_configlet:
+              try:
+                new_resp = module.client.api.add_configlet(configlet['name'],configlet['config'])
+              except Exception as error:
+                errorMessage = re.split(':', str(error))[-1]
+                message = "Configlet %s cannot be created - %s"%(configlet['name'],errorMessage)
+                created.append({configlet['name']:message})
+              else:
+                if "errorMessage" in str(new_resp):
+                    message = "Configlet %s cannot be created - %s"%(configlet['name'],new_resp['errorMessage'])
+                    new.append({configlet['name']:message})
+                else:
+                    module.client.api.add_note_to_configlet(new_resp,"## Managed by Ansible ##")
+                    changed = True
+                    new.append({configlet['name']:"success"})
+
+        # Get any Pending Tasks in CVP
+        if changed:
+            # Allow CVP to generate Tasks
+            sleep(10)
+            # Build required data for tasks in CVP - work order Id, current task status, name
+            # description
+            tasksField = {'workOrderId':'workOrderId','workOrderState':'workOrderState',
+                  'currentTaskName':'currentTaskName','description':'description',
+                  'workOrderUserDefinedStatus':'workOrderUserDefinedStatus','note':'note',
+                  'taskStatus':'taskStatus', 'workOrderDetails': 'workOrderDetails'}
+            tasks = module.client.api.get_tasks_by_status('Pending')
+            # Reduce task data to required fields
+            for task in tasks:
+                taskFacts= {}
+                for field in task.keys():
+                    if field in tasksField:
+                        taskFacts[tasksField[field]] = task[field]
+                taskList.append(taskFacts)
+        data = {'new':new,'updated':updated,'deleted':deleted,'tasks':taskList}
+    else:
+        for configlet in new_configlet:
+            new.append({configlet['name']:"checked"})
+        for configlet in update_configlet:
+            updated.append({configlet['data']['name']:"checked"})
+        for configlet in delete_configlet:
+            deleted.append({configlet['name']:"checked"})
+        data = {'new':new,'updated':updated,'deleted':deleted,'tasks':taskList}
+    return [changed,data]
+
+
+def main():
+    """ main entry point for module execution
+    """
+    argument_spec = dict(
+        host=dict(required=True),
+        port=dict(type='int', default=None),
+        protocol=dict(default='https', choices=['http', 'https']),
+        username=dict(required=True),
+        password=dict(required=True, no_log=True),
+        configlets=dict(type='dict',required=True),
+        cvp_facts=dict(type='dict',required=True),
+        configlet_filter=dict(type='list', default='none')
+        )
+
+    module = AnsibleModule(argument_spec=argument_spec,
+                           supports_check_mode=True)
+    result = dict(changed=False,data={})
+    messages = dict(issues=False)
+    # Connect to CVP instance
+    module.client = connect(module)
+
+    # Pass module params to configlet_action to act on configlet
+    result['changed'],result['data'] = configlet_action(module)
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
