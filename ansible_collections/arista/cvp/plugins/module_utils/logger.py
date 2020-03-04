8bea92e780103bb4af5ed8a5342b29ef10319e73
Thomas Grimonet <tgrimonet@arista.com>
2020-03-04 13:19:27 +0100

    Implement Log rotating in logging (#124)
    Configure default handler to run log rotation by default to avoid to
    saturate disk storage.

--- ansible_collections/arista/cvp/plugins/module_utils/logger.py
+++ ansible_collections/arista/cvp/plugins/module_utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 
 # Get Logging level from Environment variable / Default INFO
@@ -14,25 +15,14 @@ LOGLEVEL = LEVELS.get(LOGGING_LEVEL, logging.NOTSET)
 LOGGING_FILENAME = os.getenv(
     'ANSIBLE_CVP_LOG_FILE', '/temp/arista.cvp.debug.log')
 
-# set up logging to file - see previous section for more details
-logging.basicConfig(level=LOGLEVEL,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename=LOGGING_FILENAME,
-                    filemode='w')
-
 # set a format which is simpler for console use
 formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 
-# Now, we can log to the root logger, or any other logger. First the root...
-# logging.info('Jackdaws love my big sphinx of quartz.')
-
-# Now, define a couple of other loggers which might represent areas in your
-
-# logger1 = logging.getLogger('myapp.area1')
-# logger2 = logging.getLogger('myapp.area2')
-
-# logger1.debug('Quick zephyrs blow, vexing daft Jim.')
-# logger1.info('How quickly daft jumping zebras vex.')
-# logger2.warning('Jail zesty vixen who grabbed pay from quack.')
-# logger2.error('The five boxing wizards jump quickly.')
+# set up ROOT handler to use logging with file rotation.
+handler = logging.handlers.RotatingFileHandler(
+    LOGGING_FILENAME, maxBytes=1000000, backupCount=5)
+handler.setFormatter(formatter)
+handler.setLevel(LOGLEVEL)
+# Unset default logging level for root handler
+logging.getLogger('').setLevel(logging.NOTSET)
+logging.getLogger('').addHandler(handler)
