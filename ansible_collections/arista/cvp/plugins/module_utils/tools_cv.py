aristanetworks/ansible-cvp

commit 51d34efdec48cc36b11ac77a802e9e4ed5ddc538

added hashlib and difflib import tests to tools_cv and cv_configlet_v3

ansible_collections/arista/cvp/plugins/module_utils/tools_cv.py
@@ -32,6 +32,17 @@ except ImportError:
     HAS_CVPRAC = False
     CVPRAC_IMP_ERR = traceback.format_exc()
 
+try:
+    import hashlib
+    HAS_HASHLIB = True
+except ImportError:
+    HAS_HASHLIB = False
+
+try:
+    import difflib
+    HAS_DIFFLIB = True
+except ImportError:
+    HAS_DIFFLIB = False
 
 LOGGER = logging.getLogger('arista.cvp.cv_tools')
 

ansible_collections/arista/cvp/plugins/modules/cv_configlet_v3.py
@@ -110,6 +110,14 @@ def check_import(ansible_module: AnsibleModule):
         ansible_module.fail_json(
             msg='cvprac required for this module. Please install using pip install cvprac')
 
+    if not tools_cv.HAS_HASHLIB:
+        ansible_module.fail_json(
+            msg='hashlib required for this module. Please install using pip install hashlib')
+
+    if not tools_cv.HAS_DIFFLIB:
+        ansible_module.fail_json(
+            msg='difflib required for this module. Please install using pip install difflib')
+
     if not schema.HAS_JSONSCHEMA:
         ansible_module.fail_json(
             msg="JSONSCHEMA is required. Please install using pip install jsonschema")

commit e8de2d0304e0a308234533024b12bc40b6b474ae

corrected errors introduced in 51d34ef

ansible_collections/arista/cvp/plugins/module_utils/tools_cv.py
@@ -32,18 +32,6 @@ except ImportError:
     HAS_CVPRAC = False
     CVPRAC_IMP_ERR = traceback.format_exc()
 
-try:
-    import hashlib
-    HAS_HASHLIB = True
-except ImportError:
-    HAS_HASHLIB = False
-
-try:
-    import difflib
-    HAS_DIFFLIB = True
-except ImportError:
-    HAS_DIFFLIB = False
-
 LOGGER = logging.getLogger('arista.cvp.cv_tools')
 
 

ansible_collections/arista/cvp/plugins/modules/cv_configlet_v3.py
@@ -88,7 +88,7 @@ import ansible_collections.arista.cvp.plugins.module_utils.logger   # noqa # pyl
 from ansible_collections.arista.cvp.plugins.module_utils.response import CvAnsibleResponse
 import ansible_collections.arista.cvp.plugins.module_utils.tools_cv as tools_cv
 import ansible_collections.arista.cvp.plugins.module_utils.schema_v3 as schema
-from ansible_collections.arista.cvp.plugins.module_utils.configlet_tools import ConfigletInput, CvConfigletTools
+from ansible_collections.arista.cvp.plugins.module_utils.configlet_tools import ConfigletInput, CvConfigletTools, HAS_HASHLIB, HAS_DIFFLIB
 try:
     from cvprac.cvp_client_errors import CvpClientError, CvpApiError, CvpRequestError  # noqa # pylint: disable=unused-import
     HAS_CVPRAC = True
@@ -110,11 +110,11 @@ def check_import(ansible_module: AnsibleModule):
         ansible_module.fail_json(
             msg='cvprac required for this module. Please install using pip install cvprac')
 
-    if not tools_cv.HAS_HASHLIB:
+    if not HAS_HASHLIB:
         ansible_module.fail_json(
             msg='hashlib required for this module. Please install using pip install hashlib')
 
-    if not tools_cv.HAS_DIFFLIB:
+    if not HAS_DIFFLIB:
         ansible_module.fail_json(
             msg='difflib required for this module. Please install using pip install difflib')
 