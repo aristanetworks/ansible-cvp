aristanetworks/ansible-cvp

commit 51d34efdec48cc36b11ac77a802e9e4ed5ddc538

added hashlib and difflib import tests to tools_cv and cv_configlet_v3

ansible_collections/arista/cvp/plugins/module_utils/tools_cv.py
@@ -32,6 +32,17 @@ except ImportError:
     HAS_CVPRAC = False
     CVPRAC_IMP_ERR = traceback.format_exc()
 
+try:
+    import hashlib
+    HAS_HASHLIB = True
+except ImportError:
+    HAS_HASHLIB = False
+
+try:
+    import difflib
+    HAS_DIFFLIB = True
+except ImportError:
+    HAS_DIFFLIB = False
 
 LOGGER = logging.getLogger('arista.cvp.cv_tools')
 

ansible_collections/arista/cvp/plugins/modules/cv_configlet_v3.py
@@ -110,6 +110,14 @@ def check_import(ansible_module: AnsibleModule):
         ansible_module.fail_json(
             msg='cvprac required for this module. Please install using pip install cvprac')
 
+    if not tools_cv.HAS_HASHLIB:
+        ansible_module.fail_json(
+            msg='hashlib required for this module. Please install using pip install hashlib')
+
+    if not tools_cv.HAS_DIFFLIB:
+        ansible_module.fail_json(
+            msg='difflib required for this module. Please install using pip install difflib')
+
     if not schema.HAS_JSONSCHEMA:
         ansible_module.fail_json(
             msg="JSONSCHEMA is required. Please install using pip install jsonschema")

commit b453adfffd097e658b16f3e18435ea98234ecbfd

feat(cv_configlet_v3): Performance improvements in configlet tools compare functions (#369)
* substituted difflib ratios with hashes

* Improved performance of v3 configlet module

* Adds conditional check if configlet contents changed before
adding configlet to update list.

* Changed loggers to align with preexisting syntax

* added back deleted line break

* added hashlib and difflib import tests to tools_cv and cv_configlet_v3

* corrected errors introduced in 51d34ef

ansible_collections/arista/cvp/plugins/module_utils/configlet_tools.py
@@ -46,6 +46,11 @@ try:
     HAS_DIFFLIB = True
 except ImportError:
     HAS_DIFFLIB = False
+try:
+    import hashlib
+    HAS_HASHLIB = True
+except ImportError:
+    HAS_HASHLIB = False
 
 MODULE_LOGGER = logging.getLogger('arista.cvp.configlet_tools_v3')
 MODULE_LOGGER.info('Start cv_container_v3 module execution')
@@ -106,10 +111,9 @@ class CvConfigletTools(object):
     def _compare(self, fromText: List[str], toText: List[str], fromName: str = 'CVP', toName: str = 'Ansible', lines: int = 10):
         """
         _compare - Compare text string in 'fromText' with 'toText' and produce
-            diffRatio - a score as a float in the range [0, 1] 2.0*M / T
-            T is the total number of elements in both sequences,
-            M is the number of matches.
-            Score - 1.0 if the sequences are identical, and 0.0 if they have nothing in common.
+            a boolean to indicate if there is a diff between them, along with
+            a unified diff list.
+            Boolean - False if the sequences are identical, True if they are not.
             unified diff list
             Code	Meaning
             '- '	line unique to sequence 1
@@ -121,9 +125,14 @@ class CvConfigletTools(object):
         tolines = self._str_cleanup_line_ending(content=toText).splitlines(1)
         diff = list(difflib.unified_diff(
             fromlines, tolines, fromName, toName, n=lines))
-        textComp = difflib.SequenceMatcher(None, fromText, toText)
-        diffRatio = textComp.ratio()
-        return [diffRatio, diff]
+        # Calculate and compare hash values to produce the boolean.
+        fromHash = hashlib.sha1(fromText.encode()).hexdigest()
+        toHash = hashlib.sha1(toText.encode()).hexdigest()
+        if fromHash == toHash:
+            cfglet_changed = False
+        else:
+            cfglet_changed = True
+        return [cfglet_changed, diff]
 
     def is_present(self, configlet_name: str):
         """
@@ -195,7 +204,8 @@ class CvConfigletTools(object):
                     configlet['key'] = cv_data['key']
                     configlet['diff'] = self._compare(
                         fromText=cv_data['config'], toText=configlet['config'], fromName='CVP', toName='Ansible')
-                    to_update.append(configlet)
+                    if configlet['diff'][0] == True:
+                        to_update.append(configlet)
                 else:
                     to_create.append(configlet)
         elif present is False:
@@ -326,10 +336,12 @@ class CvConfigletTools(object):
                         # Save configlet diff
                         change_response.add_entry('configlet updated')
                         if 'diff' in configlet:
-                            # Change changed flag if diff is not 1.0
-                            if configlet['diff'] is not None and configlet['diff'][0] < 1.0:
+                            # Change changed flag if diff is True
+                            if configlet['diff'] is not None and configlet['diff'][0] == True:
                                 change_response.diff = configlet['diff']
                                 change_response.changed = True
+                                MODULE_LOGGER.info(
+                                    'Found diff in configlet %s.', str(configlet['name']))
                         # Collect generated tasks
                         if 'taskIds' in update_resp and len(update_resp['taskIds']) > 0:
                             change_response.taskIds = update_resp['taskIds']

ansible_collections/arista/cvp/plugins/module_utils/tools.py
@@ -28,11 +28,17 @@ try:
 except ImportError:
     HAS_DIFFLIB = False
 
+try:
+    import hashlib
+    HAS_HASHLIB = True
+except ImportError:
+    HAS_HASHLIB = False
+
+
 LOGGER = logging.getLogger('arista.cvp.tools')
 # replacement strings
 WINDOWS_LINE_ENDING = '\r\n'
 UNIX_LINE_ENDING = '\n'
-
 
 def str_cleanup_line_ending(content):
     """
@@ -57,11 +63,10 @@ def str_cleanup_line_ending(content):
 
 def compare(fromText, toText, fromName='', toName='', lines=10):
     """ Compare text string in 'fromText' with 'toText' and produce
-        diffRatio - a score as a float in the range [0, 1] 2.0*M / T
-          T is the total number of elements in both sequences,
-          M is the number of matches.
-          Score - 1.0 if the sequences are identical, and 0.0 if they have nothing in common.
-          unified diff list
+          a boolean to indicate if there is a diff between them, along
+          with a unified diff list.
+          Boolean - False if the sequences are identical, True if they are not.
+          Unified diff list:
           Code    Meaning
           '- '    line unique to sequence 1
           '+ '    line unique to sequence 2
@@ -72,9 +77,14 @@ def compare(fromText, toText, fromName='', toName='', lines=10):
     tolines = str_cleanup_line_ending(content=toText).splitlines(1)
     diff = list(difflib.unified_diff(
         fromlines, tolines, fromName, toName, n=lines))
-    textComp = difflib.SequenceMatcher(None, fromText, toText)
-    diffRatio = textComp.ratio()
-    return [diffRatio, diff]
+    # Calculate and compare hash values to produce the boolean.
+    fromHash = hashlib.sha1(fromText.encode()).hexdigest()
+    toHash = hashlib.sha1(toText.encode()).hexdigest()
+    if fromHash == toHash:
+        cfglet_changed = False
+    else:
+        cfglet_changed = True
+    return [cfglet_changed, diff]
 
 
 def isIterable(testing_object=None):

ansible_collections/arista/cvp/plugins/module_utils/tools_cv.py
@@ -32,7 +32,6 @@ except ImportError:
     HAS_CVPRAC = False
     CVPRAC_IMP_ERR = traceback.format_exc()
 
-
 LOGGER = logging.getLogger('arista.cvp.cv_tools')
 
 

ansible_collections/arista/cvp/plugins/modules/cv_configlet.py
@@ -225,20 +225,22 @@ def build_configlets_list(module):
                         ansible_configlet = module.params['configlets'][configlet['name']]
                         configlet_compare = tools.compare(
                             configlet['config'], ansible_configlet, 'CVP', 'Ansible')
-                        # compare function returns a floating point number
-                        if configlet_compare[0] == 1.0:
+                        # Compare function returns a list containing a boolean and a unified diff list.
+                        if configlet_compare[0] == False:
                             intend['keep'].append({'data': configlet})
+                            MODULE_LOGGER.debug("No change was detected in configlet %s.", str(configlet['name']))
                         else:
+                            MODULE_LOGGER.debug("Updating configlet %s due to a detected change.", str(configlet['name']))
                             intend['update'].append(
                                 {'data': configlet, 'config': ansible_configlet, 'diff': ''.join(configlet_compare[1])})
-                    # Mark configlet to be removed as module mode is absent
+                    # Mark configlet to be removed as module mode is absent.
                     elif module.params['state'] == 'absent':
                         intend['delete'].append({'data': configlet})
                 # Configlet is not in expected topology and match filter.
                 else:
                     intend['delete'].append({'data': configlet})
 
-    # Look for new configlets, if a configlet is not CVP assume it has to be created
+    # Look for new configlets, if a configlet is not CVP assume it has to be created.
     for ansible_configlet in module.params['configlets']:
         found = False
         for cvp_configlet in module.params['cvp_facts']['configlets']:
@@ -682,6 +684,9 @@ def main():
 
     if not tools.HAS_DIFFLIB:
         module.fail_json(msg='difflib required for this module')
+
+    if not tools.HAS_HASHLIB:
+        module.fail_json(msg='hashlib required for this module')
 
     if not tools_cv.HAS_CVPRAC:
         module.fail_json(

ansible_collections/arista/cvp/plugins/modules/cv_configlet_v3.py
@@ -88,7 +88,7 @@ import ansible_collections.arista.cvp.plugins.module_utils.logger   # noqa # pyl
 from ansible_collections.arista.cvp.plugins.module_utils.response import CvAnsibleResponse
 import ansible_collections.arista.cvp.plugins.module_utils.tools_cv as tools_cv
 import ansible_collections.arista.cvp.plugins.module_utils.schema_v3 as schema
-from ansible_collections.arista.cvp.plugins.module_utils.configlet_tools import ConfigletInput, CvConfigletTools
+from ansible_collections.arista.cvp.plugins.module_utils.configlet_tools import ConfigletInput, CvConfigletTools, HAS_HASHLIB, HAS_DIFFLIB
 try:
     from cvprac.cvp_client_errors import CvpClientError, CvpApiError, CvpRequestError  # noqa # pylint: disable=unused-import
     HAS_CVPRAC = True
@@ -109,6 +109,14 @@ def check_import(ansible_module: AnsibleModule):
     if HAS_CVPRAC is False:
         ansible_module.fail_json(
             msg='cvprac required for this module. Please install using pip install cvprac')
+
+    if not HAS_HASHLIB:
+        ansible_module.fail_json(
+            msg='hashlib required for this module. Please install using pip install hashlib')
+
+    if not HAS_DIFFLIB:
+        ansible_module.fail_json(
+            msg='difflib required for this module. Please install using pip install difflib')
 
     if not schema.HAS_JSONSCHEMA:
         ansible_module.fail_json(