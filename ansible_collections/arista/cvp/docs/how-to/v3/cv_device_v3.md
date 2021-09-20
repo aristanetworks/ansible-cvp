3834fe97951341d1a0beefe98998c44b4ffcbc62
Thomas Grimonet <tgrimonet@arista.com>
2021-09-20 08:35:37 +0200

    Feat(cv_device_v3): Add serialNumber support to manage devices. (#374)
    * feat(json_schema): Extend schema for anyOf fdqdn or serialNumber keys
    
    * feat(cv_device_v3): Enable serialNumber support for configlet add
    
    * feat(cv_device_v3): Enable serialNumber support for configlet remove + deploy
    
    * feat(cv_device_v3): Configure module to support serialNumber
    
    * fix(cv_device_v3.py): Change incorrect docstring
    
    * feat(cv_device_tools): Add check_mode getter&setter for CvDeviceTools
    
    * ci(pytest): Update pytest for CvDeviceTools
    
    * ci(pytest): Add debug helper env for local run of pytest
    
    * ci(pytest): Update device inventory for invalid use case
    
    * doc(mkdocs): Refresh module documentation
    
    * doc(mkdocs): Update contributing link
    
    * Bump(cvprac): Update cvprac version to >1.0.7
    
    * Make: Fix JS for mkdoc linting
    
    * Fix(cv_device_v3): Update logic to apply_configlets to support serialNumber key
    
    * Fix(device_tools): Update DeviceElement to support HOSTNAME field
    
    * Fix(device_tools): set search_mode as default for object from user in manager entrypoint
    
    * Fix(cv_device_v3): Remove duplicate call to manager
    
    * Fix(device_tools): Update remove_configlet to support serialNumber
    
    * Make(actions): Update galaxy_importer to build collection
    
    * Make(actions): Deactivate galaxy_importer as it is broken
    
    * Fix(device_tools): Change incorrect IF to ELIF
    
    * Doc(cv_device_v3): Update with new options

--- ansible_collections/arista/cvp/docs/how-to/v3/cv_device_v3.md
+++ ansible_collections/arista/cvp/docs/how-to/v3/cv_device_v3.md
@@ -24,6 +24,7 @@ CVP_DEVICES:
     configlets:
         - '01TRAINING-01'
     systemMacAddress: '50:8d:00:e3:78:aa'
+    serialNumber: 64793E1D3DE2240F547E5964354214A4
 ```
 
 ### Module inputs
@@ -38,6 +39,11 @@ CVP_DEVICES:
 - `apply_mode`: Define how configlets configured to the devices are managed by ansible:
   - `loose` (default): Configure new configlets to device and __ignore__ configlet already configured but not listed.
   - `strict`: Configure new configlets to device and __remove__ configlet already configured but not listed.
+- `search_key`: Define key to use to search for devices.
+  - `hostname`: Use Hostname to get devices.
+  - `fqdn`: Use Hostname + DNS to get devices.
+  - `serialNumber`: Use device serial number to get devices.
+
 
 ```yaml
 # Use default loose apply_mode
@@ -51,6 +57,15 @@ CVP_DEVICES:
   arista.cvp.cv_device_v3:
     devices: "{{CVP_DEVICES}}"
     apply_mode: strict
+  register: CVP_DEVICES_RESULTS
+
+# Use serial-number to search for devices.
+- name: "Configure devices on {{inventory_hostname}}"
+  arista.cvp.cv_device_v3:
+    devices: "{{CVP_DEVICES}}"
+    state: present
+    apply_mode: loose
+    search_key: serialNumber
   register: CVP_DEVICES_RESULTS
 ```
 
@@ -100,5 +115,4 @@ msg:
   success: true
   taskIds:
   - '469'
-
 ```
--- tests/unit/test_jsonSchema.py
+++ tests/unit/test_jsonSchema.py
@@ -12,7 +12,7 @@ sys.path.append("../")
 sys.path.append("../../")
 import logging
 from ansible_collections.arista.cvp.plugins.module_utils.schema_v3 import validate_cv_inputs
-from ansible_collections.arista.cvp.plugins.module_utils.schema_v3 import SCHEMA_CV_CONTAINER
+from ansible_collections.arista.cvp.plugins.module_utils.schema_v3 import SCHEMA_CV_CONTAINER, SCHEMA_CV_DEVICE
 import pytest
 
 # pytest - -html = report.html - -self-contained-html - -cov = . --cov-report = html - -color yes containerInputs.py - v
@@ -31,6 +31,17 @@ CV_CONTAINERS_KO = [
         "parentContainerName": "PYTEST", "configlets": ["01TRAINING-01"]}, "POD01": {"parentContainerName": "Leafs"}}
 ]
 
+CV_DEVICE_OK = [
+    [{"fqdn":"CV-ANSIBLE-EOS01","systemMacAddress":"50:8d:00:e3:78:aa","parentContainerName":"ANSIBLE","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}],
+    [{"serialNumber":"79AEA53101E7340AEC9AA4819D5E1F5B","systemMacAddress":"50:8d:00:e3:78:aa","parentContainerName":"ANSIBLE","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}],
+    [{"fqdn":"DEMO","serialNumber":"79AEA53101E7340AEC9AA4819D5E1F5B","systemMacAddress":"50:8d:00:e3:78:aa","parentContainerName":"ANSIBLE","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}]
+]
+
+CV_DEVICE_KO = [
+    [{"systemMacAddress":"50:8d:00:e3:78:aa","parentContainerName":"ANSIBLE","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}],
+    [{"serialNumber":"79AEA53101E7340AEC9AA4819D5E1F5B","systemMacAddress":"50:8d:00:e3:78:aa","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}],
+    [{"fqdn":"DEMO","serialNumber":"79AEA53101E7340AEC9AA4819D5E1F5B","systemMacAddress":"50:8d:00:e3:78:aa","configlets":["01TRAINING-01","CV-EOS-ANSIBLE01"],"imageBundle":[]}]
+]
 
 # ---------------------------------------------------------------------------- #
 #   PARAMETRIZE Management
@@ -43,6 +54,12 @@ def get_container_definition_valid():
 def get_container_definition_invalid():
     return CV_CONTAINERS_KO
 
+def get_device_definition_valid():
+    return CV_DEVICE_OK
+
+
+def get_device_definition_invalid():
+    return CV_DEVICE_KO
 
 # ---------------------------------------------------------------------------- #
 #   PYTEST
@@ -50,7 +67,7 @@ def get_container_definition_invalid():
 
 @pytest.mark.parametrize('CV_CONTAINER', get_container_definition_valid())
 @pytest.mark.generic
-class TestJsonSchema():
+class TestJsonSchemaContainer():
 
     def test_container_schema_valid(self, CV_CONTAINER):
         result = validate_cv_inputs(
@@ -64,4 +81,22 @@ class TestJsonSchema():
             user_json=CV_CONTAINER_INVALID, schema=SCHEMA_CV_CONTAINER)
         assert result is False
         logging.info(
-            'Topology {} is INVALID against SCHEMA_CV_CONTAINER'.format(CV_CONTAINER))
+            'Topology {} is INVALID against SCHEMA_CV_CONTAINER'.format(CV_CONTAINER_INVALID))
+
+@pytest.mark.parametrize('CV_DEVICE', get_device_definition_valid())
+@pytest.mark.generic
+class TestJsonSchemaDevice():
+
+    def test_device_schema_valid(self, CV_DEVICE):
+        result = validate_cv_inputs(
+            user_json=CV_DEVICE, schema=SCHEMA_CV_DEVICE)
+        assert result
+        logging.info('Topology {} is valid against SCHEMA_CV_DEVICE'.format(CV_DEVICE))
+
+    @pytest.mark.parametrize('CV_DEVICE_INVALID', get_device_definition_invalid())
+    def test_device_schema_invlaid(self, CV_DEVICE, CV_DEVICE_INVALID):
+        result = validate_cv_inputs(
+            user_json=CV_DEVICE_INVALID, schema=SCHEMA_CV_DEVICE)
+        assert result is False
+        logging.info(
+            'Topology {} is INVALID against SCHEMA_CV_DEVICE'.format(CV_DEVICE_INVALID))
