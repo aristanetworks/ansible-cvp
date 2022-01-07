ebadb69b40f7573778ad744d2699198418cfaccf
Matthieu Tâche <mtache@arista.com>
2022-01-07 12:19:46 +0100

    Pytest(cv_container_v3): Add unit tests for build_topology()
    Omit Ansible check_mode testing for now

---
+++ tests/unit/test_cv_container_tools.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+
+import logging
+import pytest
+import pprint
+from ansible_collections.arista.cvp.plugins.module_utils.container_tools import CvContainerTools
+from tests.lib import mock
+from tests.unit import data
+
+LOGGER = logging.getLogger(__name__)
+
+# ---------------------------------------------------------------------------- #
+#   FIXTURES
+# ---------------------------------------------------------------------------- #
+
+@pytest.fixture
+def cvp_database():
+    database = mock.MockCVPDatabase()
+    LOGGER.info('Initial CVP state: %s', database)
+    yield database
+    LOGGER.info('Final CVP state: %s', database)
+
+@pytest.fixture(params=[False]) # Fixture parameter is Ansible check_mode
+def container_tools(request, cvp_database):
+    cvp_client = mock.get_cvp_client(cvp_database)
+    module = mock.get_ansible_module(check_mode=request.param)
+    instance = CvContainerTools(cv_connection=cvp_client, ansible_module=module)
+    yield instance
+    LOGGER.debug('Mock calls: %s', pprint.pformat(cvp_client.mock_calls))
+
+# ---------------------------------------------------------------------------- #
+#   TEST CASES
+# ---------------------------------------------------------------------------- #
+
+# build_topology()
+
+@pytest.mark.generic
+@pytest.mark.parametrize("present, apply_mode, cvp_data, user_topology, expected_response", data.USER_TOPOLOGY)
+def test_build_topology(cvp_database, container_tools, present, apply_mode, cvp_data, user_topology, expected_response):
+    cvp_database.devices.update(cvp_data.get('devices', []))
+    cvp_database.containers.update(cvp_data.get('containers', []))
+    cvp_database.configlets.update(cvp_data.get('configlets', []))
+    response = container_tools.build_topology(user_topology, present = present, apply_mode = apply_mode)
+    assert response.content == expected_response
