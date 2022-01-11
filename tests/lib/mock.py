f9ba2888e328c76e87cfc59b4a3ecf1788b82b8f
Matthieu Tâche <mtache@arista.com>
2022-01-11 14:31:36 +0100

    Refactor[pytest]: Moved MockCVPDatabase related constants into class definition

--- tests/lib/mock.py
+++ tests/lib/mock.py
@@ -9,28 +9,6 @@ from ansible.module_utils.basic import AnsibleModule
 
 LOGGER = logging.getLogger(__name__)
 
-# Fields in API data
-FIELD_COUNT_DEVICES = 'childNetElementCount'
-FIELD_COUNT_CONTAINERS = 'childContainerCount'
-FIELD_PARENT_ID = 'parentContainerId'
-FIELD_NAME = 'name'
-FIELD_KEY = 'key'
-FIELD_TOPOLOGY = 'topology'
-
-# Fields in mock database
-FIELD_CONTAINER_ATTACHED = 'containerAttached'
-
-# Data used in mock methods
-CONTAINER_KEY = 'container_1234abcd-1234-abcd-12ab-123456abcdef'
-
-# Data used to initiate the mock database
-CVP_DATA_CONTAINERS_INIT = {
-    'Tenant': {
-        'name': 'Tenant',
-        'key': 'root',
-        'parentContainerId': None}
-}
-
 
 class CvpNotFoundError(Exception):
     """Exception class to be raised when data is not found in mock CVP database"""
@@ -40,22 +18,44 @@ class CvpNotFoundError(Exception):
 class MockCVPDatabase:
     """Class to mock CVP database being modified during tests"""
 
+    # Fields in API data
+    FIELD_COUNT_DEVICES = 'childNetElementCount'
+    FIELD_COUNT_CONTAINERS = 'childContainerCount'
+    FIELD_PARENT_ID = 'parentContainerId'
+    FIELD_NAME = 'name'
+    FIELD_KEY = 'key'
+    FIELD_TOPOLOGY = 'topology'
+
+    # Fields in mock database
+    FIELD_CONTAINER_ATTACHED = 'containerAttached'
+
+    # Data used in mock methods
+    CONTAINER_KEY = 'container_1234abcd-1234-abcd-12ab-123456abcdef'
+
+    # Data used to initiate the mock database
+    CVP_DATA_CONTAINERS_INIT = {
+        'Tenant': {
+            'name': 'Tenant',
+            'key': 'root',
+            'parentContainerId': None}
+    }
+
     def __init__(self, devices: dict = None, containers: dict = None, configlets: dict = None):
         self.devices = devices if devices is not None else {}
-        self.containers = containers if containers is not None else CVP_DATA_CONTAINERS_INIT.copy()
+        self.containers = containers if containers is not None else MockCVPDatabase.CVP_DATA_CONTAINERS_INIT.copy()
         self.configlets = configlets if configlets is not None else {}
         self.taskIdCounter = 0
 
     def _get_container_by_key(self, key: str) -> dict:
         for container in self.containers.values():
-            if container.get(FIELD_KEY) == key:
+            if container.get(MockCVPDatabase.FIELD_KEY) == key:
                 return container
         raise CvpNotFoundError(f'Container with key {key} not found')
 
     def _count_container_child(self, id: str) -> int:
         count = 0
         for container in self.containers.values():
-            if container[FIELD_PARENT_ID] == id:
+            if container[MockCVPDatabase.FIELD_PARENT_ID] == id:
                 count += 1
         return count
 
@@ -72,29 +72,29 @@ class MockCVPDatabase:
         # get_container_by_name() returns None if the container does not exist
         if name not in self.containers:
             return None
-        keys = [FIELD_KEY, FIELD_NAME]
+        keys = [MockCVPDatabase.FIELD_KEY, MockCVPDatabase.FIELD_NAME]
         return {k: v for k, v in self.containers[name].items() if k in keys}
 
     def add_container(self, container_name, parent_name, parent_key):
         """Mock cvprac.cvp_client.CvpApi.add_container() method"""
         if parent_name not in self.containers:
             raise CvpNotFoundError(f'Container {parent_name} not found')
-        self.containers[container_name] = {FIELD_NAME: container_name,
-                                           FIELD_KEY: CONTAINER_KEY,
-                                           FIELD_PARENT_ID: parent_key}
+        self.containers[container_name] = {MockCVPDatabase.FIELD_NAME: container_name,
+                                           MockCVPDatabase.FIELD_KEY: MockCVPDatabase.CONTAINER_KEY,
+                                           MockCVPDatabase.FIELD_PARENT_ID: parent_key}
         return self._get_response(True)
 
     def filter_topology(self, node_id='root', fmt='topology', start=0, end=0):
         """Mock cvprac.cvp_client.CvpApi.filter_topology() method"""
-        if fmt != FIELD_TOPOLOGY or start != 0 or end != 0:
+        if fmt != MockCVPDatabase.FIELD_TOPOLOGY or start != 0 or end != 0:
             raise NotImplementedError('Mock filter_topology() called with unsupported arguments')
         container = self._get_container_by_key(node_id)
-        return {FIELD_TOPOLOGY: {
-            FIELD_NAME: container[FIELD_NAME],
-            FIELD_KEY: container[FIELD_KEY],
-            FIELD_PARENT_ID: container[FIELD_PARENT_ID],
-            FIELD_COUNT_CONTAINERS: self._count_container_child(container[FIELD_KEY]),
-            FIELD_COUNT_DEVICES: 0
+        return {MockCVPDatabase.FIELD_TOPOLOGY: {
+            MockCVPDatabase.FIELD_NAME: container[MockCVPDatabase.FIELD_NAME],
+            MockCVPDatabase.FIELD_KEY: container[MockCVPDatabase.FIELD_KEY],
+            MockCVPDatabase.FIELD_PARENT_ID: container[MockCVPDatabase.FIELD_PARENT_ID],
+            MockCVPDatabase.FIELD_COUNT_CONTAINERS: self._count_container_child(container[MockCVPDatabase.FIELD_KEY]),
+            MockCVPDatabase.FIELD_COUNT_DEVICES: 0
         }
         }
 
@@ -104,12 +104,12 @@ class MockCVPDatabase:
         # We do not handle tasks here
         if not create_task:
             raise NotImplementedError('Mock apply_configlets_to_container() called with unsupported arguments')
-        if container[FIELD_NAME] not in self.containers:
-            raise CvpNotFoundError(f'Container {container[FIELD_NAME]} not found')
+        if container[MockCVPDatabase.FIELD_NAME] not in self.containers:
+            raise CvpNotFoundError(f'Container {container[MockCVPDatabase.FIELD_NAME]} not found')
         for configlet in new_configlets:
-            if configlet[FIELD_NAME] not in self.configlets:
-                raise CvpNotFoundError(f'Configlet {configlet[FIELD_NAME]} not found')
-            self.configlets[configlet[FIELD_NAME]][FIELD_CONTAINER_ATTACHED].append(container[FIELD_NAME])
+            if configlet[MockCVPDatabase.FIELD_NAME] not in self.configlets:
+                raise CvpNotFoundError(f'Configlet {configlet[MockCVPDatabase.FIELD_NAME]} not found')
+            self.configlets[configlet[MockCVPDatabase.FIELD_NAME]][MockCVPDatabase.FIELD_CONTAINER_ATTACHED].append(container[MockCVPDatabase.FIELD_NAME])
         return self._get_response(True)
 
     def __eq__(self, other):
