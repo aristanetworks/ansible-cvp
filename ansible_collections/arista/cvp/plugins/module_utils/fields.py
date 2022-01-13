f7262024e5a370ff07db7f812f2d289791104bca
Thomas Grimonet <tgrimonet@arista.com>
2022-01-13 10:46:18 +0100

    Feat(module_utils): Create initial file to centralize fields name

---
+++ ansible_collections/arista/cvp/plugins/module_utils/fields.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8 -*-
+#
+# GNU General Public License v3.0+
+#
+# Copyright 2019 Arista Networks AS-EMEA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+# Section to build FACTS dictionary
+FIELD_FACTS_DEVICE = 'cvp_devices'
+FIELD_FACTS_CONTAINER = 'cvp_containers'
+FIELD_FACTS_CONFIGLET = 'cvp_configlets'
