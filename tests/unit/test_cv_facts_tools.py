c40d7ebb63d8ea07930d57cf27e9cce76a5d67cf
Thomas Grimonet <tgrimonet@arista.com>
2022-01-14 12:22:28 +0100

    CI(pytest): Increase unit test coverage for cv_facts_v3

--- tests/unit/test_cv_facts_tools.py
+++ tests/unit/test_cv_facts_tools.py
@@ -2,48 +2,16 @@
 # coding: utf-8 -*-
 
 from __future__ import (absolute_import, division, print_function)
-import logging
 import pytest
 import pprint
 from ansible_collections.arista.cvp.plugins.module_utils.facts_tools import CvFactsTools
 from ansible_collections.arista.cvp.plugins.module_utils.fields import FIELD_FACTS_DEVICE, FIELD_FACTS_CONFIGLET, FIELD_FACTS_CONTAINER
 from tests.lib import mock
-from tests.unit import data
+from tests.lib.parametrize import generate_list_from_dict
 from tests.lib.helpers import setup_custom_logger
 from tests.lib.utils import generate_test_ids_dict, generate_test_ids_list
-# from tests.lib.cvaas_facts import FACTS_CONTAINERS_TEST, FACT_DEVICE_TEST, FACT_FILTER_TEST
 
 LOGGER = setup_custom_logger(__name__)
-
-# ---------------------------------------------------------------------------- #
-#   PARAMETRIZE
-# ---------------------------------------------------------------------------- #
-
-def generate_flat_data_from_dict(data):
-    """
-    generate_flat_data Generate a flat list of dict
-
-    Example
-    -------
-    >>> CUSTOMER_CVP_CONTAINER_TOPOLOGY = {"Tenant":{"name":"Tenant","key":"root","parentContainerId":"None"},"Undefined":{"name":"Undefined","key":"undefined_container","parentContainerId":"root"}}
-
-    >>> result = generate_flat_data(CUSTOMER_CVP_CONTAINER_TOPOLOGY)
-
-    >>> print(result)
-    [{"name":"Tenant","key":"root","parentContainerId":"None"},{"name":"Undefined","key":"undefined_container","parentContainerId":"root"}]
-
-    Parameters
-    ----------
-    data : dict
-        Data to transform
-
-    Returns
-    -------
-    list
-        List extracted from the dict
-    """
-    return [dict(data[d].items()) for d in data]
-
 
 # ---------------------------------------------------------------------------- #
 #   FIXTURES
@@ -69,15 +37,19 @@ def fact_unit_tools(request, cvp_database):
 #   TEST CASES
 # ---------------------------------------------------------------------------- #
 
+# CvFactsTools.__get_container_name
+
 @pytest.mark.generic
 @pytest.mark.usefixtures("fact_unit_tools")
 @pytest.mark.usefixtures("cvp_database")
-@pytest.mark.parametrize("container_def", generate_flat_data_from_dict(mock.MockCVPDatabase.CVP_DATA_CONTAINERS_INIT), ids=generate_test_ids_dict)
+@pytest.mark.parametrize("container_def", generate_list_from_dict(mock.MockCVPDatabase.CVP_DATA_CONTAINERS_INIT), ids=generate_test_ids_dict)
 def test_CvFactsTools__get_container_name(fact_unit_tools, container_def):
     result = fact_unit_tools._CvFactsTools__get_container_name(key=container_def[mock.MockCVPDatabase.FIELD_KEY])
-    LOGGER.debug('facts_tool response: %s', str(result))
+    LOGGER.info('facts_tool response: %s', str(result))
     assert result == container_def['name']
     LOGGER.info('Container name for id %s is %s', str(container_def[mock.MockCVPDatabase.FIELD_KEY]), str(result))
+
+# CvFactsTools.__configletIds_to_configletName
 
 @pytest.mark.generic
 @pytest.mark.usefixtures("fact_unit_tools")
@@ -99,3 +71,81 @@ def test_CvFactsTools__configletIds_to_configletName_empty(fact_unit_tools):
     LOGGER.info('** facts_tool response: %s', str(result))
     assert  result == []
     LOGGER.info('Configlet name for id [] is %s', str(result))
+
+# CvFactsTools.__device_get_configlets
+
+@pytest.mark.generic
+@pytest.mark.usefixtures("fact_unit_tools")
+@pytest.mark.usefixtures("cvp_database")
+@pytest.mark.parametrize("device_def", mock.MockCVPDatabase.CVP_DATA_DEVICE_INIT, ids=generate_test_ids_list)
+def test_CvFactsTools__device_get_configlets_with_configlets(fact_unit_tools, device_def):
+    LOGGER.info('** Sending request to get configlets name for device name: %s', str(device_def['hostname']))
+    result = fact_unit_tools._CvFactsTools__device_get_configlets(netid=device_def['systemMacAddress'])
+    LOGGER.info('** facts_tool response: %s', str(result))
+    if device_def['systemMacAddress'] in [x['objectId'] for x in mock.MockCVPDatabase.CVP_DATA_CONFIGLETS_MAPPERS_INIT['data']['configletMappers']]:
+        assert len(result) > 0
+        LOGGER.info('Device name %s has configlets %s attached', str(device_def['hostname']), str(result))
+    else:
+        pytest.skip("skipping DEVICES with no configlet")
+
+@pytest.mark.generic
+@pytest.mark.usefixtures("fact_unit_tools")
+@pytest.mark.usefixtures("cvp_database")
+@pytest.mark.parametrize("device_def", mock.MockCVPDatabase.CVP_DATA_DEVICE_INIT, ids=generate_test_ids_list)
+def test_CvFactsTools__device_get_configlets_no_configlet(fact_unit_tools, device_def):
+    LOGGER.info('** Sending request to get configlets name for device name: %s', str(device_def['hostname']))
+    result = fact_unit_tools._CvFactsTools__device_get_configlets(netid=device_def['systemMacAddress'])
+    LOGGER.info('** facts_tool response: %s', str(result))
+    if device_def['systemMacAddress'] not in [x['objectId'] for x in mock.MockCVPDatabase.CVP_DATA_CONFIGLETS_MAPPERS_INIT['data']['configletMappers']]:
+        assert len(result) == 0
+        LOGGER.info('Device has no configlet configured as expected')
+    else:
+        pytest.skip("skipping DEVICES with configlet(s)")
+
+# CvFactsTools.__device_get_configlets
+
+@pytest.mark.generic
+@pytest.mark.usefixtures("fact_unit_tools")
+@pytest.mark.usefixtures("cvp_database")
+@pytest.mark.parametrize("container_def", generate_list_from_dict(mock.MockCVPDatabase.CVP_DATA_CONTAINERS_INIT), ids=generate_test_ids_dict)
+def test_CvFactsTools__device_get_configlets_with_configlets(fact_unit_tools, container_def):
+    LOGGER.info('** Sending request to get configlets name for device name: %s', str(container_def[mock.MockCVPDatabase.FIELD_NAME]))
+    result = fact_unit_tools._CvFactsTools__containers_get_configlets(container_id=container_def[mock.MockCVPDatabase.FIELD_KEY])
+    LOGGER.info('** facts_tool response: %s', str(result))
+    if container_def[mock.MockCVPDatabase.FIELD_KEY] in [(x['objectId'], x['containerId']) for x in mock.MockCVPDatabase.CVP_DATA_CONFIGLETS_MAPPERS_INIT['data']['configletMappers']]:
+        assert len(result) > 0
+        LOGGER.info('Container name for id %s has configlets %s attached', str(container_def[mock.MockCVPDatabase.FIELD_NAME]), str(result))
+    else:
+        pytest.skip("skipping CONTAINER with no configlet")
+
+@pytest.mark.generic
+@pytest.mark.usefixtures("fact_unit_tools")
+@pytest.mark.usefixtures("cvp_database")
+@pytest.mark.parametrize("container_def", generate_list_from_dict(mock.MockCVPDatabase.CVP_DATA_CONTAINERS_INIT), ids=generate_test_ids_dict)
+def test_CvFactsTools__device_get_configlets_with_no_configlets(fact_unit_tools, container_def):
+    LOGGER.info('** Sending request to get configlets name for device name: %s', str(container_def[mock.MockCVPDatabase.FIELD_NAME]))
+    result = fact_unit_tools._CvFactsTools__containers_get_configlets(container_id=container_def[mock.MockCVPDatabase.FIELD_KEY])
+    LOGGER.info('** facts_tool response: %s', str(result))
+    if container_def[mock.MockCVPDatabase.FIELD_KEY] in [(x['objectId'], x['containerId']) for x in mock.MockCVPDatabase.CVP_DATA_CONFIGLETS_MAPPERS_INIT['data']['configletMappers']]:
+        assert len(result) == 0
+        LOGGER.info('Container name %s has no configlets %s attached', str(container_def[mock.MockCVPDatabase.FIELD_NAME]), str(result))
+    else:
+        pytest.skip("skipping CONTAINER with configlet(s)")
+
+
+# CvFactsTools.__device_update_info
+
+@pytest.mark.generic
+@pytest.mark.usefixtures("fact_unit_tools")
+@pytest.mark.usefixtures("cvp_database")
+@pytest.mark.parametrize("device_def", mock.MockCVPDatabase.CVP_DATA_DEVICE_INIT, ids=generate_test_ids_list)
+def test_CvFactsTools__device_get_configlets_no_configlet(fact_unit_tools, device_def):
+    LOGGER.info('** Sending request to get update device info for: %s', str(device_def['hostname']))
+    result = fact_unit_tools._CvFactsTools__device_update_info(device=device_def)
+    LOGGER.info('Updated device information: %s', str(result))
+    assert mock.MockCVPDatabase.FIELD_PARENT_NAME in result
+    if device_def['parentContainerKey'] != '':
+        assert result[mock.MockCVPDatabase.FIELD_PARENT_NAME] in [x['name'] for x in generate_list_from_dict(mock.MockCVPDatabase.CVP_DATA_CONTAINERS_INIT)]
+        LOGGER.info('Device is attached to container %s', str(result[mock.MockCVPDatabase.FIELD_PARENT_NAME]))
+    else:
+        LOGGER.warning('Device is not attached to any container')
