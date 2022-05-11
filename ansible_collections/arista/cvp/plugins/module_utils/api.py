5a0e8a2d6c4d9b50cb44b74505e6a0b32a20f93d
Matthieu Tâche <mtache@arista.com>
2022-05-11 21:45:51 +0200

    Introduce pagination_coeff value in call_batch(). The item_per_call is now depending on the CPU count and the total number of items to get from server.

--- ansible_collections/arista/cvp/plugins/module_utils/api.py
+++ ansible_collections/arista/cvp/plugins/module_utils/api.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8 -*-
-#
-# GNU General Public License v3.0+
-#
-# Copyright 2019 Arista Networks AS-EMEA
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+#!/usr/bin/python
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -22,6 +14,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import logging
+import os
 import asyncio
 import time
 import functools
@@ -54,7 +47,7 @@ async def call(func: Callable[..., dict], args: List[dict]) -> List[dict]:
     List[dict]
         Aggregated results of concurrent API calls
     """
-    now = time.time()
+    started_at = time.monotonic()
     loop = asyncio.get_running_loop()
 
     LOGGER.info('%s: Querying %s items', func.__name__, len(args))
@@ -64,12 +57,12 @@ async def call(func: Callable[..., dict], args: List[dict]) -> List[dict]:
                                            functools.partial(func, **kwargs))
                                            for kwargs in args))
 
-    LOGGER.info('%s: Queried %s items in %ss', func.__name__, len(responses), time.time() - now)
+    LOGGER.info('%s: Queried %s items in %ss', func.__name__, len(responses), time.monotonic() - started_at)
 
     return responses
 
 
-async def call_batch(func: Callable[[int, int], dict], item_per_call: int = 2) -> dict:
+async def call_batch(func: Callable[[int, int], dict], pagination_coeff: int = 4) -> dict:
     """
     Query CloudVision using concurrent API calls.
     Take as argument a function that have the following signature: func(start:int, end:int).
@@ -84,7 +77,11 @@ async def call_batch(func: Callable[[int, int], dict], item_per_call: int = 2) -
 
     This function can be a blocking call since it is run in a separate thread.
 
-    The optional parameter item_per_call defines the number of item to retrieve per concurrent API call.
+    The optional parameter pagination_coeff influences the number of item retrieved per concurrent API call.
+    A pagination_coeff value of 1 means the number API calls will be equal equal to the max_workers value on
+    the execution machine. A value of 2 means the number of API calls will be half the max_workers value.
+    In Python 3.8, max_workers value is min(32, os.cpu_count() + 4. This value defines the maximum of running
+    concurrent threads of a ThreadPoolExecutor.
 
     This is a coroutine and can be used in asynchronous code to query multiple ressources concurently.
 
@@ -93,14 +90,14 @@ async def call_batch(func: Callable[[int, int], dict], item_per_call: int = 2) -
     func : Callable[[int, int], dict]
         Function to call
     item_per_call : int, optional
-        Number of item to retrieve per concurrent API call, by default 2
+        Number of item to retrieve per concurrent API call
 
     Returns
     -------
     dict
         Aggregated results of concurrent API calls
     """
-    now = time.time()
+    started_at = time.monotonic()
     data = []
     loop = asyncio.get_running_loop()
     first = func(start=0, end=1)
@@ -108,13 +105,17 @@ async def call_batch(func: Callable[[int, int], dict], item_per_call: int = 2) -
     data.extend(first['data'])
 
     if total == 1:
-        LOGGER.info('%s: Collected 1 item in %ss', func.__name__, time.time() - now)
+        LOGGER.info('%s: Collected 1 item in %ss', func.__name__, time.monotonic() - started_at)
         return {'total': total, 'data': data}
 
     # Some cvprac calls are broken and ignore the start and end parameters
     if len(data) == total:
-        LOGGER.info('%s: Collected %s items in %ss', func.__name__, len(data), time.time() - now)
+        LOGGER.info('%s: Collected %s items in %ss', func.__name__, len(data), time.monotonic() - started_at)
         return {'total': total, 'data': data}
+
+    if not item_per_call:
+        # min(32, os.cpu_count() + 4) is the max_workers value of ThreadPoolExecutor in Python 3.8
+        item_per_call = int(total / min(32, os.cpu_count() + 4) * pagination_coeff)
 
     LOGGER.info('%s: Collecting %s items, %s items per API call', func.__name__, total, item_per_call)
 
@@ -125,7 +126,7 @@ async def call_batch(func: Callable[[int, int], dict], item_per_call: int = 2) -
 
     for r in responses:
         data.extend(r['data'])
-    LOGGER.info('%s: Collected %s items in %ss', func.__name__, len(data), time.time() - now)
+    LOGGER.info('%s: Collected %s items in %ss', func.__name__, len(data), time.monotonic() - started_at)
 
     return {'total': total, 'data': data}
 
@@ -134,16 +135,18 @@ def get_configlets_by_name(client, names: List[str]) -> List[dict]:
     return asyncio.run(call(client.api.get_configlet_by_name, [{'name': i} for i in names]))
 
 
-def get_configlets(client, **item_per_call) -> dict:
-    return asyncio.run(call_batch(client.api.get_configlets, **item_per_call))
 
 
-def get_images(client, **item_per_call) -> dict:
-    return asyncio.run(call_batch(client.api.get_images, **item_per_call))
+def get_configlets(client) -> dict:
+    return asyncio.run(call_batch(client.api.get_configlets))
 
 
-def get_containers(client, **item_per_call) -> dict:
-    return asyncio.run(call_batch(client.api.get_containers, **item_per_call))
+def get_images(client) -> dict:
+    return asyncio.run(call_batch(client.api.get_images))
+
+
+def get_containers(client) -> dict:
+    return asyncio.run(call_batch(client.api.get_containers))
 
 
 def update_configlets(client, configs: List[dict]) -> dict:
