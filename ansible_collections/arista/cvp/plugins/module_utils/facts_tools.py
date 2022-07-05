8f1a0f42f4fdfc246f2aaa67b0b6f3bb9c511361
Colin MacGiollaEain <colin@arista.com>
2022-07-05 14:43:54 +0100

    troubleshooting

--- ansible_collections/arista/cvp/plugins/module_utils/facts_tools.py
+++ ansible_collections/arista/cvp/plugins/module_utils/facts_tools.py
@@ -454,7 +454,7 @@ class CvFactsTools():
                 MODULE_LOGGER.debug('Got following information for container: %s', str(container))
                 container[Api.generic.CONFIGLETS] = self.__containers_get_configlets(container_id=container[Api.container.KEY])
                 container[Api.container.IMAGE_BUNDLE] = self.__cv_client.api.get_image_bundle_by_container_id(container[Api.container.KEY])
-                MODULE_LOGGER.debug('%s container is assigned to %s', (str(container[Api.generic.NAME]), str(container[Api.container.IMAGE_BUNDLE])))
+                MODULE_LOGGER.debug('The following bundle is assigned: %s' % str(self.__cv_client.api.get_image_bundle_by_container_id(container[Api.container.KEY])))
                 facts_builder.add(container)
         self._facts[FactsResponseFields.CONTAINER] = facts_builder.get(resource_model='container')
 
