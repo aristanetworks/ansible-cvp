72d25cd6601cfde8536d9ebf089baf4b8efdd3e0
Colin MacGiollaEain <colin@flat-planet.net>
2022-08-16 11:32:48 +0100

    Rename generic IMAGE_BUNDLE Cleanup trailing whitespace Explicitly set type as 'netelement' when assigining image

--- ansible_collections/arista/cvp/plugins/module_utils/resources/api/fields.py
+++ ansible_collections/arista/cvp/plugins/module_utils/resources/api/fields.py
@@ -47,7 +47,7 @@ class ApiGeneric():
     NAME: str = 'name'
     PARENT_CONTAINER_ID: str = 'parentContainerId'
     PARENT_CONTAINER_NAME: str = 'parentContainerName'
-    IMAGE_BUNDLE: str = 'imageBundle'
+    IMAGE_BUNDLE_NAME: str = 'imageBundle'
 
 
 # @dataclass
