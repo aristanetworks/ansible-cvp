e0c3ac78450efd5e372d8dcb60366b92d12444ce
Colin MacGiollaEain <colin@arista.com>
2022-11-03 10:19:36 +0000

    Activate image bundle management

--- ansible_collections/arista/cvp/plugins/module_utils/container_tools.py
+++ ansible_collections/arista/cvp/plugins/module_utils/container_tools.py
@@ -986,10 +986,16 @@ class CvContainerTools(object):
         return self.__configlet_del(container=container_info, configlets=detach_configlets)
 
     def image_bundle_attach(self, container: str, image_name: str):
-
         container_info = self.get_container_info(container_name=container)
         MODULE_LOGGER.debug("Attempting to apply image bundle %s to container %s", str(image_name),str(container))
         return self.__image_bundle_add(container=container_info, image_bundle=image_name)
+
+
+    def image_bundle_detach(self, container: str):
+        container_info = self.get_container_info(container_name=container)
+        MODULE_LOGGER.debug("Attampting to remove image bundle from %s", str(container))
+        return self.__image_bundle_del(container=container_info)
+
 
 
     def build_topology(self, user_topology: ContainerInput, present: bool = True, apply_mode: str = 'loose'):
@@ -1025,7 +1031,10 @@ class CvContainerTools(object):
             builder_name=ContainerResponseFields.CONFIGLETS_ATTACHED)
         cv_configlets_detach = CvManagerResult(
             builder_name=ContainerResponseFields.CONFIGLETS_DETACHED, default_success=True)
-
+        cv_image_bundle_attach = CvManagerResult(
+            builder_name=ContainerResponseFields.BUNDLE_ATTACHED)
+        cv_image_bundle_detach = CvManagerResult(
+            builder_name=ContainerResponseFields.BUNDLE_DETACHED)
         try:
 
             # Create containers topology in Cloudvision
@@ -1059,6 +1068,17 @@ class CvContainerTools(object):
                         if len(configlet_to_remove) > 0:
                             resp = self.configlets_detach(container=user_container, configlets=configlet_to_remove)
                             cv_configlets_detach.add_change(resp)
+                    
+                    if user_topology.has_image_bundle(container_name=user_container):
+                        MODULE_LOGGER.debug('%s container has an image bundle assigned', str(user_container))
+                        resp = self.image_bundle_attach(
+                            container=user_container, image_name=user_topology.get_image_bundle(container_name=user_container))
+                        cv_image_bundle_attach.add_change(resp)
+                    elif apply_mode == ModuleOptionValues.APPLY_MODE_STRICT:
+                        container_facts = self.get_container_id(container_name=user_container)
+                        if container_facts[API.generic.IMAGE_BUNDLE_NAME] is not None:
+                            resp = self.image_bundle_detach(container=user_container)
+                            cv_image_bundle_detach.add_change(resp)
 
             else:
                 for user_container in reversed(user_topology.ordered_list_containers):
