b98f285a23959ee40174b8371bf0a02bd4c72853
Sugetha Chandhrasekar <sugethakch@arista.com>
2023-02-06 14:54:07 -0800

    Test: adding library files for unittests

---
+++ tests/unit/conftest.py
@@ -0,0 +1,28 @@
+from unittest.mock import create_autospec
+import pytest
+from tests.lib import mockMagic
+from cvprac.cvp_client import CvpClient, CvpApi
+
+
+@pytest.fixture
+def apply_mock(mocker):
+    """
+    apply_mock - factory function to return a method to apply mocker.patch() on paths
+
+    """
+    def _apply_mock_factory(paths: list):
+        return [mocker.patch(path) for path in paths]
+
+    return _apply_mock_factory
+
+@pytest.fixture
+def mock_cvpClient():
+    """
+    mock_cvprac - mocks cvprac classes/objects
+    """
+    # mocked cvpClient object
+    mock_cvpClient = create_autospec(CvpClient)
+    mock_cvpClient.api = mockMagic.MockCvpApi(spec=CvpApi)
+    # mock_cvpClient.api = create_autospec(CvpApi)
+    # mock_cvpClient.api.validate_config_for_device.side_effect = dummy_cvprac.validate_config_for_device
+    return mock_cvpClient
