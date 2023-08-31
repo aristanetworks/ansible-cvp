36d6236e9df1849282b9a2de5247c25e833d0780
Tamas Plugor <tamas@arista.com>
2023-08-31 16:34:15 +0100

    add fixtures and read vars from it + cleanup

--- ansible_collections/arista/cvp/molecule/cv_device_v3/reconcile.py
+++ ansible_collections/arista/cvp/molecule/cv_device_v3/reconcile.py
@@ -3,28 +3,37 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 from cvprac.cvp_client import CvpClient
-import ssl
 import yaml
-ssl._create_default_https_context = ssl._create_unverified_context
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
+
+# Setting variables
+RECONCILE = 'RECONCILE_' # Prefix for the configlet name
+fixture_file = 'molecule/fixtures/cv_device_v3.yaml'
+
+with open(fixture_file, encoding="utf-8") as f:
+    dut = yaml.safe_load(f)
+
+# load password from fixtures otherwise assume it's and ATD environemnt and read the password from
+# the config file
+if len(dut[0]["password"]) > 0:
+    password = dut[0]["password"]
+else:
+    config_file = "/home/coder/.config/code-server/config.yaml"
+    with open(config_file, encoding="utf-8") as f:
+        password = yaml.safe_load(f)["password"]
+
+# Connect to CloudVision
 clnt = CvpClient()
 clnt.set_log_level(log_level='WARNING')
-
-# load the ATD password from the code-server config file
-# to test this in another environment this var will need to be updated
-with open("/home/coder/.config/code-server/config.yaml", encoding="utf-8") as f:
-    password = yaml.safe_load(f)["password"]
-
-# Connect to CVP
-clnt.connect(['192.168.0.5'],'arista', password)
+clnt.connect([dut[0]["node"]], dut[0]["username"], password)
 
 # Store device information
-device = clnt.api.get_device_by_serial('s1-leaf1')
-dev_mac = device['systemMacAddress']
+device = clnt.api.get_device_by_serial(dut[0]["device"])
+dev_mac = device["systemMacAddress"]
 
 # Reconcile device configuration
 rc = clnt.api.get_device_configuration(dev_mac)
-name = 'RECONCILE_' + device['serialNumber']
+name = RECONCILE + device['serialNumber']
 update = clnt.api.update_reconcile_configlet(dev_mac, rc, "", name, True)
-addcfg = clnt.api.apply_configlets_to_device("auto-reconciling",device,[update['data']])
+addcfg = clnt.api.apply_configlets_to_device("auto-reconciling", device,[update['data']])
