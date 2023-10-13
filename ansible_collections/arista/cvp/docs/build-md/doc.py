b2579a1564fa2e244ed0429966584587a13d2263
juliopdx <juliop503@gmail.com>
2023-10-13 12:23:50 -0700

    Non required shebang

--- ansible_collections/arista/cvp/docs/build-md/doc.py
+++ ansible_collections/arista/cvp/docs/build-md/doc.py
@@ -1,21 +1,3 @@
-#!/usr/bin/python
-# coding: utf-8 -*-
-#
-# Copyright 2023 Arista Networks
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import os
 from pathlib import Path
 
@@ -31,7 +13,7 @@ def jinja2_writer(template_dir, name, data):
     env = Environment(
         loader=FileSystemLoader(template_dir),
         trim_blocks=True,
-        keep_trailing_newline=True
+        keep_trailing_newline=True,
     )
 
     template = env.get_template("md.j2")
