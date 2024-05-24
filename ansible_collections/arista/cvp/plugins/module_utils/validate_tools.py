bff47ea912f03f57f317c364c50b6f99c704f3d3
Tamas Plugor <tamas@arista.com>
2024-05-24 18:58:22 +0100

    Fix: cv_validate_v3 broken with cvprac 1.4.0 due to argument keyword change

--- ansible_collections/arista/cvp/plugins/module_utils/validate_tools.py
+++ ansible_collections/arista/cvp/plugins/module_utils/validate_tools.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2023 Arista Networks, Inc.
+# Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 # coding: utf-8 -*-
@@ -191,9 +191,7 @@ class CvValidationTools(object):
                     MODULE_LOGGER.debug(
                         "queryParams are deviceMac: %s and configuration: %s", str(
                             system_mac), str(config))
-                    resp = self.__cv_client.api.validate_config_for_device(
-                        device_mac=system_mac,
-                        config=config)
+                    resp = self.__cv_client.api.validate_config_for_device(system_mac, config)
                     result_data.add_entry(
                         configlet_name + "_validated_against_" + device_info['device_name']
                     )
